6a8ee9292f83b701707cdaffd55b9b99164be681
411426414 <411426414@qq.com>
2021-01-14 14:59:02 +0800

    添加根路由

--- file/routes/index.js
+++ file/routes/index.js
@@ -5,21 +5,10 @@ const multer = require('multer')
 const fs = require('fs')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send({
-    success:true
-  })
+router.get('/', function (req, res, next) {
+    res.send({
+        success: true
+    })
 });
-
-router.get('/index', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.post('/upload',multer({
-  dest: 'upload'
-}).single('file'),(req,res)=>{
-  console.log(req.file);
-  res.send(req.file)
-})
 
 module.exports = router;
